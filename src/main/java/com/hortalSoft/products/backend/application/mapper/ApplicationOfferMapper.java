7412a0412f2cdfb313d6d0aa9e38b9c5b5be0d03
isa-quintero <69700863+isa-quintero@users.noreply.github.com>
2024-04-09 23:53:58 -0500

    Refactorización categoryDTO

--- src/main/java/com/hortalSoft/products/backend/application/mapper/ApplicationOfferMapper.java
+++ src/main/java/com/hortalSoft/products/backend/application/mapper/ApplicationOfferMapper.java
@@ -2,7 +2,6 @@ package com.hortalSoft.products.backend.application.mapper;
 
 import com.hortalSoft.products.backend.application.dto.OfferDTO;
 import com.hortalSoft.products.backend.domain.model.Offer;
-import com.hortalSoft.products.backend.domain.model.Product;
 
 //@Mapper(componentModel = "spring", uses = {})
 public interface ApplicationOfferMapper extends ApplicationEntityMapper<OfferDTO, Offer> {
