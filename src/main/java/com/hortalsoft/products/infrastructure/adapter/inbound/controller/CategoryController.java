18f00f6b6f4fdde31347af702df0bd2da04d9412
isa-quintero <69700863+isa-quintero@users.noreply.github.com>
2024-07-15 16:13:40 -0500

    Documentación con swagger en categoria

--- src/main/java/com/hortalsoft/products/infrastructure/adapter/inbound/controller/CategoryController.java
+++ src/main/java/com/hortalsoft/products/infrastructure/adapter/inbound/controller/CategoryController.java
@@ -1,10 +1,16 @@
 package com.hortalsoft.products.infrastructure.adapter.inbound.controller;
 
 import com.hortalsoft.products.application.dto.CategoryDTO;
+import com.hortalsoft.products.application.dto.SubcategoryDTO;
 import com.hortalsoft.products.application.facades.facade.category.FindCategoryFacade;
 import com.hortalsoft.products.application.facades.facade.category.ListCategoryFacade;
 import com.hortalsoft.crosscutting.util.ExceptionHandlingAspect;
 import com.hortalsoft.crosscutting.util.ExceptionHortalsoft;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -29,6 +35,16 @@ public class CategoryController {
         this.exceptionHandlingAspect = exceptionHandlingAspect;
     }
 
+    @Operation(summary = "Encuentra una categoria por su id", description = "Regresa una categoria por un id dado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SubcategoryDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Categoria no encontrada",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error inesperado",
+                    content = @Content)
+    })
     @GetMapping("/categories/{id}")
     public ResponseEntity<Object> findCategory(@PathVariable(name = "id") int id) {
         try {
@@ -42,6 +58,16 @@ public class CategoryController {
 
     }
 
+    @Operation(summary = "Lista todas las categorias", description = "Regresa una lista con todas las categorias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categorias listadas",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SubcategoryDTO.class))),
+            @ApiResponse(responseCode = "404", description = "No hay categorias para listar",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error inesperado ",
+                    content = @Content)
+    })
     @GetMapping("/categories")
     public ResponseEntity<Object> listCategories() {
         try {
